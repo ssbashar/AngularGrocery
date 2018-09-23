6722401a6a25d47515c7df4d7894b5c182354e36
Salma Bashar <salmabashar@gmail.com>
2018-09-23 03:07:44 -0700

    Implement a grocery list app using angular-6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { GroceryComponent } from './app.grocery';
 
 @NgModule({
   declarations: [
-    AppComponent
+    GroceryComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    GroceryComponent,
+  ],
 })
 export class AppModule { }
